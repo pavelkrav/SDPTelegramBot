d7667b2cf7a7bfb7c8b1d1e2c424f82ceb3704f4
Pavel Kravtsov <pavelkr.msc@gmail.com>
2016-12-02 17:28:25 +0300

    alpha stable

--- SDPTelegramBot/SDPTelegramBot/Program.cs
+++ SDPTelegramBot/SDPTelegramBot/Program.cs
@@ -1,14 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Net;
-using Newtonsoft.Json;
-using System.IO;
-using System.Web;
-
 
 namespace SDPTelegramBot
 {
@@ -18,11 +8,6 @@ namespace SDPTelegramBot
 		{
 			Client client = new Client();
 			client.Start(0, true);
-
-			//SDPRequest req = new SDPRequest(6730);
-			//Console.WriteLine("\u041a\u0443\u043b\u0438\u043a\u043e\u0432");
-
-
 
 			Console.ReadKey();
 		}
